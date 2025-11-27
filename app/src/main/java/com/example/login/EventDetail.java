acd182b6a1fa46ad44e19fd2216465f7cc426f75
Seojin Lee <jeje0329@naver.com>
2025-11-28 03:42:22 +0900

    admin 모집event detail

--- app/src/main/java/com/example/login/EventDetail.java
+++ app/src/main/java/com/example/login/EventDetail.java
@@ -1,18 +1,27 @@
 package com.example.login;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.login.databinding.FragmentEventDetailBinding;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EventDetail extends Fragment {
     FragmentEventDetailBinding binding;
@@ -39,11 +48,10 @@ public class EventDetail extends Fragment {
         //뒤로가기 버튼
         binding.back.setOnClickListener(v-> getParentFragmentManager().popBackStack());
 
-
         //이벤트 정보 불러오기
         String docID = getArguments().getString("documentID");
 
-        /// events가 하위컬렉션이므로 코드 수정 필요함. 동아리 문서에 따라 events 하위 컬렉션이 있는거임
+        /// 각 동아리 문서의 events 하위컬렉션으로 접근할 수 있도록 변경 해야함!!!
         db.collection("events")
                 .document(docID)
                 .get()
@@ -61,6 +69,10 @@ public class EventDetail extends Fragment {
                         binding.endDate.setText(endDate);
                         binding.location.setText(location);
                         binding.content.setText(content);
+
+                        if (doc.contains("recruit num")) binding.applyInform.setVisibility(View.VISIBLE);
+                        else binding.applyInform.setVisibility(View.GONE);
+
                     }
                 })
                 .addOnFailureListener(e -> Log.w("LSJ", "fail to load document", e));
@@ -85,5 +97,87 @@ public class EventDetail extends Fragment {
                     .addToBackStack(null)
                     .commit();
         });
+
+        //모집 인원 띄우기
+        List<ApplyModel> applyList = new ArrayList<>();
+        applyList.add(new ApplyModel("신청자", "56781234", null));
+        applyList.add(new ApplyModel("신청자2", "56789101", null));
+
+
+        ApplyAdapter applyAdapter = new ApplyAdapter(requireContext(), applyList);
+
+        RecyclerView applyRecyclerView = binding.applyInform;
+        applyRecyclerView.setAdapter(applyAdapter);
+        applyRecyclerView.setLayoutManager(new LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false));
+    }
+
+    class ApplyModel{
+        private final String name;
+        private final String  studentNum;
+        private final String profileUri;
+
+        public ApplyModel(String name, String  studentNum, String profileUri){
+            this.name = name;
+            this.studentNum = studentNum;
+            this.profileUri = profileUri;
+        }
+
+        public String getName(){ return name; }
+        public String  getStudentNum(){ return studentNum; }
+        public String getProfileUri(){ return profileUri; }
+    }
+
+    public class ApplyAdapter extends RecyclerView.Adapter<ApplyAdapter.ViewHolder> {
+
+        private final List<ApplyModel> applyList;
+        private final Context context;
+
+        public ApplyAdapter(Context context, List<ApplyModel> applyList) {
+            this.context = context;
+            this.applyList = applyList;
+        }
+
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(context)
+                    .inflate(R.layout.apply_inform, parent, false);
+            return new ViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            ApplyModel model = applyList.get(position);
+            holder.bind(model);
+        }
+
+        @Override
+        public int getItemCount() {
+            return applyList.size();
+        }
+
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+            ImageView profileImage;
+            TextView name;
+            TextView studentNum;
+
+            public ViewHolder(View itemView) {
+                super(itemView);
+                profileImage = itemView.findViewById(R.id.profileImage);
+                name = itemView.findViewById(R.id.name);
+                studentNum = itemView.findViewById(R.id.studentNum);
+            }
+
+            public void bind(ApplyModel applyModel){
+                name.setText(applyModel.getName());
+                studentNum.setText(applyModel.getStudentNum());
+
+                String uri = applyModel.getProfileUri();
+                if(uri != null && !uri.isEmpty()){
+                    Uri imageUri = android.net.Uri.parse(uri);
+                    profileImage.setImageURI(imageUri);
+                } else profileImage.setImageResource(R.drawable.image);
+            }
+        }
     }
 }
