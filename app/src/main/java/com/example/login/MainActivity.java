11f0b5b9d874bb982c855411f861a84a85900ed8
Seojin Lee <jeje0329@naver.com>
2025-12-06 17:37:22 +0900

    commit

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -118,7 +118,7 @@ public class MainActivity extends AppCompatActivity {
 
                                 // 관리자 확인 완료 → 관리자 화면 이동
                                 Log.d("LSJ", "Admin login success");
-                                startActivity(new Intent(MainActivity.this,AdminMyPageActivity.class));
+                                startActivity(new Intent(MainActivity.this, AdminMyPageActivity.class));
                             })
                             .addOnFailureListener(e -> {
                                 Log.w("LSJ", "Error checking admin status", e);
